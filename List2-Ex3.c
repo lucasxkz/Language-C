21fbcbefd2bb1ec55d1941bb2ba064300cb0e367
Lucas de Oliveira <102926785+lucasxkz@users.noreply.github.com>
2022-10-27 08:00:07 -0300

    Create List2-Ex3.c

---
+++ List2-Ex3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+int main (void){
+    int ano;
+    printf ("insira o ano de referencia: ");
+    scanf("%d", &ano);
+    if (ano%100!=0 && ano %4==0){
+        printf(" o ano é bissexto");
+    return 0;
+    }
+    else(ano);
+        printf("não é bissexto.");
+    return 0;
+return 0;
+}
