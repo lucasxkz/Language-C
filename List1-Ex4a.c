137fe758f3504499a673f01596afed2162ea10f9
Lucas de Oliveira <102926785+lucasxkz@users.noreply.github.com>
2022-10-27 07:53:51 -0300

    Create List1-Ex4a.c

---
+++ List1-Ex4a.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <math.h>
+int main (void){
+    int nbit;
+    nbit = pow (2,4);
+    printf ("A quantidade de valores em um binario de 4 bits sera igual a %d", nbit);
+    return 0;
+}
